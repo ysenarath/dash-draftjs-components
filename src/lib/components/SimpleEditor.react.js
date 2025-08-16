12560341d6528979a1ed611238772739996b8c00
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-08-16 00:07:24 -0400

    Enhance SimpleEditor to support inline style toggling and update prop names for consistency

--- src/lib/components/SimpleEditor.react.js
+++ src/lib/components/SimpleEditor.react.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import {Editor, EditorState, RichUtils} from 'draft-js';
@@ -6,17 +6,13 @@ import 'draft-js/dist/Draft.css';
 import './SimpleEditor.css';
 
 /**
- * ExampleComponent is an example component.
- * It takes a property, `label`, and
- * displays it.
- * It renders an input with the property `value`
- * which is editable by the user.
+ * A simple text editor component using Draft.js.
  */
 const SimpleEditor = (props) => {
-    const {id, setProps, editorState, placeholder } = props;
+    const {id, setProps, editor_state, placeholder, toggle_inline_style} = props;
 
     const onChange = (newEditorState) => {
-        setProps({ editorState: newEditorState });
+        setProps({ editor_state: newEditorState });
     };
 
     const handleKeyCommand = (command, editorState) => {
@@ -28,33 +24,69 @@ const SimpleEditor = (props) => {
         }
     
         return 'not-handled';
-      }
+      };
 
-    const _onBoldClick = () => {
-    onChange(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
-    }
+
+    // listen for button clicks to toggle bold style
+    useEffect(() => {
+        if (toggle_inline_style !== null) {
+            if (toggle_inline_style === "CODE") {
+                onChange(RichUtils.toggleCode(editor_state));
+            } else {
+                onChange(RichUtils.toggleInlineStyle(editor_state, toggle_inline_style));
+            }
+            setProps({ toggle_inline_style: null });
+        }
+    }, [toggle_inline_style]);
     
 
     return (
         <div id={id} className="dash-draft-editor">
-            <button onClick={_onBoldClick}>Bold</button>
-            <hr />
-            <Editor editorState={editorState}  onChange={onChange} placeholder={placeholder} handleKeyCommand={handleKeyCommand} />
+            <Editor 
+                editorState={editor_state}  
+                onChange={onChange} 
+                placeholder={placeholder} 
+                handleKeyCommand={handleKeyCommand} 
+            />
         </div>
     );
 }
 
 SimpleEditor.defaultProps = {
-    editorState: EditorState.createEmpty()
+    toggle_inline_style: null,
+    editor_state: EditorState.createEmpty(),
+    placeholder: 'Type something...'
 };
 
 SimpleEditor.propTypes = {
+    /**
+     * The unique identifier for the component.
+     */
     id: PropTypes.string,
 
-    editorState: PropTypes.object,
+    /**
+    * The current state of the editor.
+    * This should be an instance of `EditorState`.
+    */
+    editor_state: PropTypes.object,
 
+    /**
+     * Placeholder text for the editor when it is empty.
+     * This is displayed when the editor is focused and has no content.
+     */
     placeholder: PropTypes.string,
 
+    /**
+     * Number of times the bold button has been clicked.
+     * This is used to toggle the bold style in the editor.
+     */
+    toggle_inline_style: PropTypes.string,
+
+    /**
+     * Function to set properties in the parent component.
+     * This is used to update the editor state and other properties.
+     * @param {Object} props - The properties to set.
+     */
     setProps: PropTypes.func
 };
 
