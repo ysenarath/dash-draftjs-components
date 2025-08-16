101b6d6e424404c3e7bc4cd8b11f62cedc4369e7
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-08-16 00:07:28 -0400

    Add watch scripts to package.json and implement inline style toggling in usage.py

--- usage.py
+++ usage.py
@@ -1,3 +1,4 @@
+import json
 import dash_draftjs_components as ddc
 from dash import Dash, callback, html, Input, Output
 
@@ -5,6 +6,8 @@ app = Dash(__name__)
 
 app.layout = html.Div(
     [
+        html.Button("Bold", id="bold-button", n_clicks=0),
+        html.Button("Code", id="code-button", n_clicks=0),
         ddc.SimpleEditor(id="input"),
         html.Div(id="output"),
     ]
@@ -13,13 +16,31 @@ app.layout = html.Div(
 
 @callback(
     Output("output", "children"),
-    Input("input", "editorState"),
+    Input("input", "editor_state"),
 )
 def update_output(editor_state):
     if editor_state is None:
         return "No content"
-    print(editor_state)
+    # return html.Pre(json.dumps(editor_state, indent=2))
     return []
+
+
+@callback(
+    Output("input", "toggle_inline_style", allow_duplicate=True),
+    Input("bold-button", "n_clicks"),
+    prevent_initial_call=True,
+)
+def update_bold_click_count(n_clicks):
+    return "BOLD"
+
+
+@callback(
+    Output("input", "toggle_inline_style"),
+    Input("code-button", "n_clicks"),
+    prevent_initial_call=True,
+)
+def update_code_click_count(n_clicks):
+    return "CODE"
 
 
 if __name__ == "__main__":
