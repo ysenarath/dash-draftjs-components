3c62d1a7acf4ee6ff5de735c447ddd3abb7cb31d
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-08-15 22:48:09 -0400

    Replace Editor component with SimpleEditor and update callback to handle editorState

--- usage.py
+++ usage.py
@@ -5,15 +5,21 @@ app = Dash(__name__)
 
 app.layout = html.Div(
     [
-        ddc.Editor(id="input", value="my-value", label="my-label"),
+        ddc.SimpleEditor(id="input"),
         html.Div(id="output"),
     ]
 )
 
 
-@callback(Output("output", "children"), Input("input", "value"))
-def display_output(value):
-    return "You have entered {}".format(value)
+@callback(
+    Output("output", "children"),
+    Input("input", "editorState"),
+)
+def update_output(editor_state):
+    if editor_state is None:
+        return "No content"
+    print(editor_state)
+    return []
 
 
 if __name__ == "__main__":
