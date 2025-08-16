f1288a98b64b2243efd1c690ecca785f21beebfd
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-08-15 21:50:31 -0400

    Add initial build

---
+++ R/internal.R
@@ -0,0 +1,15 @@
+.dashDraftjsComponents_js_metadata <- function() {
+deps_metadata <- list(`dash_draftjs_components` = structure(list(name = "dash_draftjs_components",
+version = "0.0.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'dash_draftjs_components.min.js',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "dashDraftjsComponents",
+all_files = FALSE), class = "html_dependency"),
+`dash_draftjs_components` = structure(list(name = "dash_draftjs_components",
+version = "0.0.1", src = list(href = NULL,
+file = "deps"), meta = NULL,
+script = 'dash_draftjs_components.min.js.map',
+stylesheet = NULL, head = NULL, attachment = NULL, package = "dashDraftjsComponents",
+all_files = FALSE, dynamic = TRUE), class = "html_dependency"))
+return(deps_metadata)
+}
